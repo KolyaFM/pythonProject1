b49375611f365339712d05d7e54835f2b5638396
KolyaFM <kolyafmmix@gmail.com>
2023-02-08 14:00:58 +0300

    second commit

--- main.py
+++ main.py
@@ -12,7 +12,4 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('фФФФ')
-    a['3'](54)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-   "kolyafmmix@gmail.com"
